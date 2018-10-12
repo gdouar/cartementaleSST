640afd9b09d80f09657c90fc97f62667013bf4a6
Pyhro36 <pylou_11@hotmail.fr>
2018-10-12 10:33:11 +0200

    add mind map

---
+++ cartementaleSST/mindmap.mm
@@ -0,0 +1,83 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Nouveaux standards et architectures de r&#xe9;f&#xe9;rence pour l&apos;industrie 4.0" FOLDED="false" ID="ID_368675033" CREATED="1539019012744" MODIFIED="1539019114256" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Architecture d&apos;entreprise" POSITION="right" ID="ID_403698410" CREATED="1539019120424" MODIFIED="1539019915348">
+<edge COLOR="#ff0000"/>
+<node TEXT="Archiecture h&#xe9;rit&#xe9;e" ID="ID_1234950861" CREATED="1539019722119" MODIFIED="1539019844420" HGAP_QUANTITY="19.999999821186073 pt" VSHIFT_QUANTITY="-20.24999939650299 pt">
+<node TEXT="Framework d&apos;architecture d&apos;entreprise" ID="ID_1324981538" CREATED="1539019142946" MODIFIED="1539019157993"/>
+<node TEXT="Architecture de r&#xe9;f&#xe9;rence" ID="ID_1851385299" CREATED="1539019169935" MODIFIED="1539019178769"/>
+<node TEXT="Organisation de l&apos;entreprise" ID="ID_371304535" CREATED="1539019193897" MODIFIED="1539019201821"/>
+<node TEXT="Architecture logicielle" ID="ID_1120662536" CREATED="1539019297687" MODIFIED="1539019865627"/>
+<node TEXT="Business Intelligence" ID="ID_449876657" CREATED="1539019312132" MODIFIED="1539019873700"/>
+<node TEXT="Business Digitization" ID="ID_1598700241" CREATED="1539019321161" MODIFIED="1539019876584"/>
+</node>
+<node TEXT="Transformations/am&#xe9;liorations" ID="ID_968556386" CREATED="1539019883259" MODIFIED="1539020784460">
+<node TEXT="Collaborative Enterprise Architecture" ID="ID_1347889144" CREATED="1539019420084" MODIFIED="1539020784460" HGAP_QUANTITY="15.499999955296522 pt" VSHIFT_QUANTITY="-11.249999664723882 pt"/>
+<node TEXT="Interoperability" ID="ID_1664877347" CREATED="1539019679210" MODIFIED="1539019686612"/>
+</node>
+</node>
+</node>
+</map>
